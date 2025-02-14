96c6a0cd6e3c088a423396d1608b621771e1a8af
Xemorph <finn.brandes@outlook.de>
2025-02-14 10:41:26 +0100

    [+](Added) Color badge support

--- js/main.js
+++ js/main.js
@@ -1,40 +1,50 @@
 const DOMAIN = "sf_symbols_svg";
-
 const ICON_STORE = {};
+const PATH_CLASSES = {
+    base: "base",
+    badge: "badge",
+  };
 
 const preProcessIcon = async (iconSet, iconName) => {
-    let icon = iconName;
+    let [icon, format] = iconName.split("#");
     const data = await fetch(`/${DOMAIN}/icons/${iconSet}/${icon}.svg`);
     const text = await data.text();
     const parser = new DOMParser();
     const doc = parser.parseFromString(text, "text/html");
 
     if (!doc || !doc.querySelector("svg")) {
-        console.log("SF Symbols SVG: invalid icon");
+        console.log(`SF Symbols SVG: Invalid icon '${icon}'`);
         return {};
     }
 
     const viewBox = doc.querySelector("svg").getAttribute("viewBox");
     const _paths = doc.querySelectorAll("path");
     const paths = {};
+
     let path = undefined;
+    let badgePath = undefined;
     let sumpath = "";
+
     for (const pth of _paths) {
         sumpath = sumpath + pth.getAttribute("d");
+
+        const cls = pth.classList[0];
+        if (PATH_CLASSES[cls] == "base") path = pth.getAttribute("d");
+        if (PATH_CLASSES[cls] == "badge") badgePath = pth.getAttribute("d");
     }
     path = path ?? sumpath;
     
-/* Javascript security context
+/* Javascript security context */
     // Don't allow full code to be used if the svg may contain javascript
     let innerSVG = doc.querySelector("svg");
     // Don't allow full code if any attribute is onClick or something
     if (Array.from(innerSVG?.attributes).some((a) => a.name.startsWith("on")))
-      innerSVG = undefined;
+        innerSVG = undefined;
     // Don't allow full code if it contains <script> tags
     if (innerSVG?.getElementsByTagName("script").length) innerSVG = undefined;
-*/
+/**/
 
-    return { viewBox, path };
+    return { viewBox, path, badgePath, paths: sumpath, format, innerSVG };
 };
 
 const getIcon = (iconSet, iconName) => {
@@ -66,3 +76,25 @@ window.customIcons["sf"] = {
     getIcon: (iconName) => getIcon("regular", iconName),
     getIconList: () => getIconList("regular"),
 };
+
+// Fullcolor support patch
+customElements.whenDefined("ha-icon").then((HaIcon) => {
+    const orig = HaIcon.prototype._setCustomPath;
+    HaIcon.prototype._setCustomPath = async function (promise, requestedIcon) {
+        await orig?.bind(this)?.(promise, requestedIcon);
+  
+        const icon = await promise;
+        if (requestedIcon !== this.icon) return;
+        if (!icon.innerSVG || icon.format !== "fullcolor") return;
+  
+        await this.UpdateComplete;
+        const el = this.shadowRoot.querySelector("ha-svg-icon");
+        await el?.updateComplete;
+  
+        this._path = undefined;
+        this._badgePath = undefined;
+  
+        const root = el?.shadowRoot.querySelector("svg");
+        root?.appendChild(icon.innerSVG.cloneNode(true));
+    };
+});
