27bc7319f1f70d40cb7bea3cd34f81b000bedbfe
Xemorph <finn.brandes@outlook.de>
2025-08-20 10:33:45 +0200

    [.](Bump) Fixed config flow

--- custom_components/sfsymbols/config_flow.py
+++ custom_components/sfsymbols/config_flow.py
@@ -9,11 +9,11 @@ _LOGGER = logging.getLogger(__name__)
 
 
 @config_entries.HANDLERS.register(DOMAIN)
-class SFSymbolsSVGConfigFlow(config_entries.ConfigFlow):
+class SFSymbolsConfigFlow(config_entries.ConfigFlow):
 
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
         if self._async_current_entries():
             return self.async_abort(reason="single_instance_allowed")
-        return self.async_create_entry(title="SF Symbols SVG", data={})
+        return self.async_create_entry(title="SF Symbols", data={})
